a917e012f0a9e645da061bc7a1d059ca776df335
Alexandre St-Onge <stonge.alex@gmail.com>
2016-06-09 10:26:35 -0400

    first commit

---
+++ FFTactics/Assets/Scripts/TileMap.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class TileMap : MonoBehaviour
+{
+
+    public float Radius = 1;
+    public float Height = 1;
+
+    public int DimX = 4;
+    public int DimZ = 4;
+
+    private const int ANGLE = 60;
+    private const float MAGIC_NUMBER = 0.866025403f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    GenerateMesh();
+	}
+
+    void GenerateMesh()
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        List<Vector3> normals = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        Vector3 center = new Vector3(0, Height, 0);
+        Vector3 xOffset = new Vector3(Radius + Radius/2.0f, 0, 0);
+        float seed = System.DateTime.Now.Second;
+        for(int x = 0; x < DimX; ++x)
+        {
+            for(int z = 0; z < DimZ; ++z)
+            {
+                center.y = Mathf.Pow(Mathf.PerlinNoise(seed + x / (float)DimX, seed + z/(float)DimZ) + 0.5f, 3) * Height;
+                Vector3[] tempVertices;
+                int[] tempTriangles;
+                Vector3[] tempNormals;
+                Vector2[] tempUvs;
+                CreateTileAround(center, out tempVertices, out tempTriangles, out tempNormals, out tempUvs);
+
+                int trianglesOffset = vertices.Count;
+                vertices.AddRange(tempVertices);
+
+                foreach(int v in tempTriangles)
+                {
+                    triangles.Add(v + trianglesOffset);
+                }
+
+                normals.AddRange(tempNormals);
+                uvs.AddRange(tempUvs);
+                center += new Vector3(0, 0, 2*Radius*MAGIC_NUMBER);
+            }
+            center += xOffset;
+            if(x%2==0)
+            {
+                center.z = MAGIC_NUMBER*Radius;
+            }
+            else
+            {
+                center.z = 0;
+            }
+        }
+
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.normals = normals.ToArray();
+        mesh.uv = uvs.ToArray();
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+    }
+
+    void CreateTileAround(Vector3 center, out Vector3[] vertices, out int[] triangles, out Vector3[] normals, out Vector2[] uvs)
+    {
+        vertices = new Vector3[6*3];
+        triangles = new int[16*3];
+        normals = new Vector3[6*3];
+        uvs = new Vector2[6*3];
+
+        Vector3 radiusVector = new Vector3(Radius, 0, 0);
+        Vector3 heightVector = new Vector3(0, center.y, 0);
+
+        for (int i = 0; i < 6; ++i)
+        {
+            
+            vertices[i] = center + radiusVector;
+            vertices[6+i] = vertices[i];
+            vertices[12+i] = vertices[i] - heightVector;
+            radiusVector = Quaternion.AngleAxis(ANGLE, Vector3.up) * radiusVector;
+
+            normals[i] = Vector3.up;
+            normals[6+i] = Vector3.Normalize(vertices[i] - center);
+            normals[12+i] = Vector3.Normalize(vertices[i] - center);
+        }
+
+        // Top hexagon
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        triangles[3] = 0;
+        triangles[4] = 2;
+        triangles[5] = 5;
+
+        triangles[6] = 5;
+        triangles[7] = 2;
+        triangles[8] = 3;
+
+        triangles[9] = 5;
+        triangles[10] = 3;
+        triangles[11] = 4;
+
+        //Top Uv
+        uvs[0] = new Vector2(.25f,1.0f);
+        uvs[1] = new Vector2(0.5f, 0.75f);
+        uvs[2] = new Vector2(0.5f, 0.25f);
+        uvs[3] = new Vector2(0.25f, 0);
+        uvs[4] = new Vector2(0, 0.25f);
+        uvs[5] = new Vector2(0, 0.75f);
+
+        //Side Uvs
+        uvs[6] = new Vector2(0.5f, 1);
+        uvs[7] = new Vector2(1, 1);
+        uvs[8] =  new Vector2(0.5f, 1);
+        uvs[9] = new Vector2(1 ,1);
+        uvs[10] =  new Vector2(0.5f, 1);
+        uvs[11] = new Vector2(1, 1);
+
+        uvs[12] = new Vector2(0.5f, 0);
+        uvs[13] = new Vector2(1, 0);
+        uvs[14] = new Vector2(.5f, 0);
+        uvs[15] = new Vector2(1, 0);
+        uvs[16] = new Vector2(0.5f, 0);
+        uvs[17] = new Vector2(1, 0);
+
+        //Side wall
+        for(int i = 0; i < 6; ++i)
+        {
+            triangles[12 + 6*i] = i+6;
+            triangles[12 + 6*i + 1]= i + 12;
+            triangles[12 + 6*i + 2]= (i + 1)%6 + 6;
+
+            triangles[12 + 6*i + 3] = (i + 1)%6 + 6;
+            triangles[12 + 6*i + 4] = i + 12;
+            triangles[12 + 6*i + 5] = (i + 1)%6 + 12;
+        }
+    }
+}
---
+++ FFTactics/Assets/Shaders/TileShader.shader
@@ -0,0 +1,32 @@
+﻿Shader "Custom/TileShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_Blink ("Freq", Float ) = 0
+		
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Lambert
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		fixed4 _Color;
+		float _Blink;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			o.Albedo = _Color;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
